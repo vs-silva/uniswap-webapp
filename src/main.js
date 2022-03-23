bfac743ab3bfd0b91944196c457a9186a3e62000
Vitalino Silva <vitalinosilva@gmail.com>
2022-03-23 15:42:25 +0000

    configurend multilang engine

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import LangResources from './langResources';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+
+createApp(App)
+    .use(LangResources)
+    .mount('#app');
