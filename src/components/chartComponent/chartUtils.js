a13f19923c9732060fb9bc42c7c787d7e64c4b2a
Vitalino Silva <vitalinosilva@gmail.com>
2022-03-24 10:41:24 +0000

    optimized chart utils implementation

--- src/components/chartComponent/chartUtils.js
+++ src/components/chartComponent/chartUtils.js
@@ -1,53 +1,83 @@
 import ChartLabels from './chartLabels';
-import { Translate } from "../../langResources";
+import ChartManager from '../../chartManager';
+import { Translate } from '../../langResources';
+import ErrorManager from '../../errorManager';
 
-function generateChartData(tokensData) {
-    const result = {
-        labels: [],
-        datasets: [{
-            label: '',
-            data: [],
-            backgroundColor: []
-        }]
-    };
+export default (props, chartContainer) => {
 
-    if(!tokensData) {
+    if(!props || !chartContainer) {
+        ErrorManager.processError({
+            error: 'Required params must be provided',
+            reporter: 'Chart Utils'
+        });
+    }
+
+    let chart;
+
+    function generateChart(charType) {
+
+        if(chart) {
+            chart.destroy();
+        }
+
+        chart = ChartManager.createChart(chartContainer.value, {
+            type: charType
+        });
+
+        updateChart(props);
+    }
+
+    function updateChart(props) {
+        const chartData = generateChartData(extractTokensData(props));
+
+        if(chart) {
+            ChartManager.updateChart(chart, chartData);
+        }
+    }
+
+    function generateChartData(tokensData) {
+        const result = {
+            labels: [],
+            datasets: [{
+                label: '',
+                data: [],
+                backgroundColor: []
+            }]
+        };
+
+        if(!tokensData) {
+            return result;
+        }
+
+        result.datasets[0].label = Translate(ChartLabels.title);
+
+        tokensData.forEach(item => {
+            result.labels.push(`${item.name} ( ${item.symbol} )`);
+            result.datasets[0].data.push(item.tvlUSD);
+            result.datasets[0].backgroundColor.push(generateRandomColor());
+        });
+
         return result;
     }
 
-    result.datasets[0].label = Translate(ChartLabels.title);
+    function generateRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
 
-    tokensData.forEach(item => {
-        result.labels.push(`${item.name} ( ${item.symbol} )`);
-        result.datasets[0].data.push(item.tvlUSD);
-        result.datasets[0].backgroundColor.push(generateRandomColor());
-    });
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
 
-    return result;
-}
-
-function generateRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+        return color;
     }
 
-    return color;
-}
+    function extractTokensData(props) {
+        const { tokensData } = JSON.parse(JSON.stringify(props));
+        return tokensData;
+    }
 
-function processPropsTokensData(props) {
-    const { tokensData } = JSON.parse(JSON.stringify(props));
-    return generateChartData(tokensData);
-}
-
-function displayChart(chartData) {
-    const { datasets } = JSON.parse(JSON.stringify(chartData));
-    return (datasets.length > 0);
-}
-
-export default {
-    processPropsTokensData,
-    displayChart
-}
+    return {
+        generateChart,
+        updateChart
+    };
+};
