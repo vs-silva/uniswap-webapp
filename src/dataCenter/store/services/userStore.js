637a96cc4e6aebee5c83c3e9e2b10a00c5baa23e
Vitalino Silva <vitalinosilva@gmail.com>
2022-03-28 19:41:42 +0100

    added store unit tests and fixed bugs

--- src/dataCenter/store/services/userStore.js
+++ src/dataCenter/store/services/userStore.js
@@ -7,14 +7,14 @@ export default (client) => {
 
     if(!client) {
         return ErrorManager.processError({
-            error: 'GraphQL client must be provided',
+            error: 'REST client must be provided',
             reporter: 'User store service'
         });
     }
 
-    const { defaultRequestParams } = Config.api.rest;
+    const defaultRequestParams = Config.api.rest;
 
-    async function getUser({resource = defaultRequestParams.resource, params = defaultRequestParams.param}) {
+    async function getUser({resource = defaultRequestParams.resource, params = defaultRequestParams.param} = {}) {
 
         const user = await client.get(UserURI({
             resource : resource,
